c1c610b8a1dc179e572ec9869a6b1d2c23c7c693
gino53 <castagnagino5@gmail.com>
2022-08-29 23:44:41 +0200

    header

--- Groupomania/src/app/app.module.ts
+++ Groupomania/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { PostComponent } from './post/post.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
 import { PostListComponent } from './post-list/post-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    PostListComponent
+    PostListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
