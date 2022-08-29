d43e8a5273bf7fe81a1f8f9bad01e32db1b4bbeb
gino53 <castagnagino5@gmail.com>
2022-08-29 23:32:26 +0200

    post list

--- Groupomania/src/app/app.component.ts
+++ Groupomania/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Post } from './models/post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,63 +6,6 @@ import { Post } from './models/post.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  posts!: Post[];
 
-  ngOnInit() {
-    this.posts = [
-      {
-        title: 'Manège',
-        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/19/01/06/ferris-wheel-7395944_960_720.jpg',
-        createdDate: new Date(),
-        description: 'Grande-roue',
-        like: 0,
-        location: 'Paris'
-      },
-
-      {
-        title: 'Papillon',
-        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/15/12/47/moth-7387787_960_720.jpg',
-        createdDate: new Date(),
-        description: 'Insecte',
-        like: 0,
-        location: 'Bordeaux'
-      },
-
-      {
-        title: 'Chien',
-        imageUrl: 'https://cdn.pixabay.com/photo/2022/07/17/12/12/pembroke-welsh-corgi-7327285_960_720.jpg',
-        createdDate: new Date(),
-        description: 'Animal de compagnie',
-        like: 0,
-        location: 'Lyon'
-      },
-
-      {
-        title: 'Manège',
-        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/19/01/06/ferris-wheel-7395944_960_720.jpg',
-        createdDate: new Date(),
-        description: 'Grande-roue',
-        like: 0,
-        location: 'Paris'
-      },
-
-      {
-        title: 'Papillon',
-        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/15/12/47/moth-7387787_960_720.jpg',
-        createdDate: new Date(),
-        description: 'Insecte',
-        like: 0,
-        location: 'Bordeaux'
-      },
-
-      {
-        title: 'Chien',
-        imageUrl: 'https://cdn.pixabay.com/photo/2022/07/17/12/12/pembroke-welsh-corgi-7327285_960_720.jpg',
-        createdDate: new Date(),
-        description: 'Animal de compagnie',
-        like: 0,
-        location: 'Lyon'
-      }
-    ];
-  }
+  ngOnInit() { }
 }
--- Groupomania/src/app/post-list/post-list.component.ts
+++ Groupomania/src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '.\models\post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
-
-  constructor() { }
+  posts!: Post[];
 
   ngOnInit(): void {
+    this.posts = [
+      {
+        title: 'Manège',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/19/01/06/ferris-wheel-7395944_960_720.jpg',
+        createdDate: new Date(),
+        description: 'Grande-roue',
+        like: 0,
+        location: 'Paris'
+      },
+
+      {
+        title: 'Papillon',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/15/12/47/moth-7387787_960_720.jpg',
+        createdDate: new Date(),
+        description: 'Insecte',
+        like: 0,
+        location: 'Bordeaux'
+      },
+
+      {
+        title: 'Chien',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/07/17/12/12/pembroke-welsh-corgi-7327285_960_720.jpg',
+        createdDate: new Date(),
+        description: 'Animal de compagnie',
+        like: 0,
+        location: 'Lyon'
+      },
+
+      {
+        title: 'Manège',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/19/01/06/ferris-wheel-7395944_960_720.jpg',
+        createdDate: new Date(),
+        description: 'Grande-roue',
+        like: 0,
+        location: 'Paris'
+      },
+
+      {
+        title: 'Papillon',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/08/15/12/47/moth-7387787_960_720.jpg',
+        createdDate: new Date(),
+        description: 'Insecte',
+        like: 0,
+        location: 'Bordeaux'
+      },
+
+      {
+        title: 'Chien',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/07/17/12/12/pembroke-welsh-corgi-7327285_960_720.jpg',
+        createdDate: new Date(),
+        description: 'Animal de compagnie',
+        like: 0,
+        location: 'Lyon'
+      }
+    ];
   }
 
 }
