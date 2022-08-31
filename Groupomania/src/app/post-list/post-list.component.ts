56070e391c6ef796ffcf8c0ae28e2b2014ff950a
gino53 <castagnagino5@gmail.com>
2022-08-31 16:51:54 +0200

    interval destroy

--- Groupomania/src/app/post-list/post-list.component.ts
+++ Groupomania/src/app/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 import { Post } from '../models/post.model';
 import { PostsService } from '../services/posts.service';
 
@@ -7,12 +9,23 @@ import { PostsService } from '../services/posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   posts!: Post[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.posts = this.postsService.getAllPosts();
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
