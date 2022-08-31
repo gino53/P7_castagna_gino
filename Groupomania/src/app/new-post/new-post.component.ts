38cf6c85bdd41d8ed9c0c2a9dd3f1ddbaf74fe1b
gino53 <castagnagino5@gmail.com>
2022-08-31 17:18:17 +0200

    new post

--- Groupomania/src/app/new-post/new-post.component.ts
+++ Groupomania/src/app/new-post/new-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-post',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  postForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.postForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null]
+    });
+  }
+
+  onSubmitForm(): void {
+    console.log(this.postForm.value);
   }
 
 }
