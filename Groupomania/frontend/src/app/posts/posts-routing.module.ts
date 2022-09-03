961e040a106135b6a9fa592685b83802d9be65b1
gino53 <castagnagino5@gmail.com>
2022-09-03 20:42:04 +0200

    guard

--- Groupomania/frontend/src/app/posts/posts-routing.module.ts
+++ Groupomania/frontend/src/app/posts/posts-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "../core/guards/auth.guard";
 import { NewPostComponent } from "./components/new-post/new-post.component";
 import { PostListComponent } from "./components/post-list/post-list.component";
 import { SinglePostComponent } from "./components/single-post/single-post.component";
 
 const routes: Routes = [
-    { path: 'create', component: NewPostComponent },
-    { path: ':id', component: SinglePostComponent},
-    { path: '', component: PostListComponent }
-]
+    { path: 'create', component: NewPostComponent, canActivate: [AuthGuard] },
+    { path: ':id', component: SinglePostComponent, canActivate: [AuthGuard] },
+    { path: '', component: PostListComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
     imports: [
