94893b06a661ef4722aa38a6e4660b7270a48227
gino53 <castagnagino5@gmail.com>
2022-09-03 20:27:55 +0200

    login

--- Groupomania/frontend/src/app/auth/components/login/login.component.ts
+++ Groupomania/frontend/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(): void {
+    this.auth.login(),
+    this.router.navigateByUrl('/posts');
   }
 
 }
