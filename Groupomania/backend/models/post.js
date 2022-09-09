ef85bd6104d5cb2d43d59dfc686a30777c2f2319
gino53 <castagnagino5@gmail.com>
2022-09-09 17:37:40 +0200

    mysql

---
+++ Groupomania/backend/models/post.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.Post.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      })
+    }
+  }
+  Post.init({
+    userId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    date: DataTypes.STRING,
+    likes: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
